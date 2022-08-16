4a1143474e8f89574798422059d6ce4121299729
JE <wjd7dms7@naver.com>
2022-08-16 11:55:36 +0900

    0816-11-55

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,39 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test("the counter starts at 0", () => {
+  render(<App />)
+  const counterElement = screen.getByTestId("counter");
+  expect(counterElement).toHaveTextContent(0);
+})
+
+test("plus button has correct text", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  const buttonElement = screen.getByTestId("plus-button");
+  expect(buttonElement).toHaveTextContent("+");
+}) // +버튼이 있는지부터 확인하는 과정
+
+test("When the + button is pressed, the counter changes to 1", () => {
+  render(<App />);
+  const buttonElement = screen.getByTestId("plus-button");
+  fireEvent.click(buttonElement);
+  const counterElement = screen.getByTestId("counter");
+  expect(buttonElement).toHaveTextContent("1");
+
+})
+
+
+test("minus button has correct text", () => {
+  render(<App />);
+  const buttonElement = screen.getByTestId("minus-button");
+  expect(buttonElement).toHaveTextContent("-");
+}) 
+
+test("When the + button is pressed, the counter changes to 1", () => {
+  render(<App />);
+  const buttonElement = screen.getByTestId("minus-button");
+  fireEvent.click(buttonElement);
+  const counterElement = screen.getByTestId("counter");
+  expect(buttonElement).toHaveTextContent("0");
+
+})
